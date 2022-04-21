6093ffa91a2e5ce607b7f3b4ae19f02452963279
Vladislav Molchanov <Molchanov96@yandex.ru>
2022-04-21 22:45:34 +0300

    #1 Правки в webpack.dev.js

--- api/webpack.dev.js
+++ api/webpack.dev.js
@@ -1,15 +1,15 @@
+/**
+ * Конфиг написан только для корректной работы WebStorm с кастомными алиасами
+ */
+
 const path = require('path');
 
 const moduleAliases = require('./package.json')._moduleAliases;
-const alias = {
-    // '@': __dirname + '/'
-};
+const alias = {};
 
 for (const key in moduleAliases) {
     alias[key] = path.resolve(__dirname, moduleAliases[key]);
 }
-
-// alias['@'] = __dirname + '/';
 
 module.exports = {
     resolve: {alias},
